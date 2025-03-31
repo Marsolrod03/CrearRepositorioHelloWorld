7ff6d1b3808657d96693351023231a5f132a95d4
mariosima1 <mariosima100@gmail.com>
2025-03-31 12:31:11 +0200

    Se crea un tercer fragment

---
+++ app/src/main/java/com/example/crearrepositorio/ThirdFragment.kt
@@ -0,0 +1,33 @@
+package com.example.crearrepositorio
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.crearrepositorio.databinding.FragmentThirdBinding
+
+class ThirdFragment : Fragment() {
+
+    private var _binding: FragmentThirdBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentThirdBinding.inflate(inflater, container, false)
+
+        binding.btnHome.setOnClickListener {
+            back()
+        }
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
