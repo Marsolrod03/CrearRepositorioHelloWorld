07d49d28f9999b9508c484d4eca516a69f9233f2
mariosima1 <mariosima100@gmail.com>
2025-04-02 11:39:48 +0200

    Optimize imports

--- app/src/main/java/com/example/crearrepositorio/ui/fragments/FragmentHome.kt
+++ app/src/main/java/com/example/crearrepositorio/ui/fragments/FragmentHome.kt
@@ -1,15 +1,14 @@
 package com.example.crearrepositorio.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import com.example.crearrepositorio.R
 import com.example.crearrepositorio.databinding.FragmentHomeBinding
 import com.example.crearrepositorio.ui.replaceFragment
 import com.example.crearrepositorio.ui.viewModel.MainViewModel
