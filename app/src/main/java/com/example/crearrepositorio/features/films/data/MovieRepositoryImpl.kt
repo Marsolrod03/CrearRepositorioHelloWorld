edf5cb717c57e1cf0835c1590a6a74d47c20933e
HugoRamiloMarcote <hugoramilo4@gmail.com>
2025-04-10 10:15:31 +0200

    Renames
    Movies adapter: Rename movies -> listMovies and fakeMovies -> movies
    Fragment_first.xml: layoutManager was deleted

--- app/src/main/java/com/example/crearrepositorio/features/films/data/MovieRepositoryImpl.kt
+++ app/src/main/java/com/example/crearrepositorio/features/films/data/MovieRepositoryImpl.kt
@@ -7,16 +7,21 @@ import kotlinx.coroutines.flow.flow
 
 class MovieRepositoryImpl : MovieRepository {
 
-    override fun createMovies(): Flow<List<MovieModel>> = flow {
-        emit(
-            listOf(
-                MovieModel("The Shawshank Redemption", "src1", listOf("Drama")),
-                MovieModel("The Dark Knight", "src2", listOf("Action", "Crime", "Drama")),
-                MovieModel("Inception", "src3", listOf("Action", "Adventure", "Sci-Fi")),
-                MovieModel("Forrest Gump", "src4", listOf("Drama", "Romance")),
-                MovieModel("The Matrix", "src5", listOf("Action", "Sci-Fi")),
-                MovieModel("Pulp Fiction", "src6", listOf("Crime", "Drama"))
+    override fun createMovies(): Flow<List<MovieModel>> {
+
+        val json_path =
+
+       return flow {
+            emit(
+                listOf(
+                    MovieModel("The Shawshank Redemption", "src1", listOf("Drama")),
+                    MovieModel("The Dark Knight", "src2", listOf("Action", "Crime", "Drama")),
+                    MovieModel("Inception", "src3", listOf("Action", "Adventure", "Sci-Fi")),
+                    MovieModel("Forrest Gump", "src4", listOf("Drama", "Romance")),
+                    MovieModel("The Matrix", "src5", listOf("Action", "Sci-Fi")),
+                    MovieModel("Pulp Fiction", "src6", listOf("Crime", "Drama"))
+                )
             )
-        )
+        }
     }
 }
