bd00b7512553c83e60320ed4f8de13db1656669e
mariosima1 <mariosima100@gmail.com>
2025-04-25 13:04:50 +0200

    Retrofit provider and ActorsNetworkDataSource added

--- app/src/main/java/com/example/crearrepositorio/features/actors/data/ActorsRepositoryImpl.kt
+++ app/src/main/java/com/example/crearrepositorio/features/actors/data/ActorsRepositoryImpl.kt
@@ -1,27 +1,17 @@
 package com.example.crearrepositorio.features.actors.data
 
-import com.example.crearrepositorio.common_data.RetrofitClient
 import com.example.crearrepositorio.features.actors.domain.ActorModel
 import com.example.crearrepositorio.features.actors.domain.ActorsRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
-class ActorsRepositoryImpl @Inject constructor(): ActorsRepository {
+class ActorsRepositoryImpl @Inject constructor
+    (private val actorsNetworkDataSource: ActorsNetworkDataSource) : ActorsRepository {
 
     override fun getActors(): Flow<List<ActorModel>> = flow {
-        val service = RetrofitClient().retrofit
-        val response = service.create(ActorsService::class.java).listActors()
-
-        if (response.isSuccessful) {
-            response.body()?.results?.let { actors ->
-                emit(actors.map { actor ->
-                    actor.toActorModel()
-                })
-            }
-        } else {
-            throw Exception("Error charging the actors")
-        }
+        val result = actorsNetworkDataSource.fetchActors()
+        emit(result.map { it.toActorModel() })
 
     }
 }
