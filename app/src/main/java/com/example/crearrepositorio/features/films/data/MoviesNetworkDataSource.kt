0dd1de925aef77ee2bf8787219c81a5580e4805c
HugoRamiloMarcote <hugoramilo4@gmail.com>
2025-04-25 13:40:41 +0200

    update MoviesNetworkDataModule

--- app/src/main/java/com/example/crearrepositorio/features/films/data/MoviesNetworkDataSource.kt
+++ app/src/main/java/com/example/crearrepositorio/features/films/data/MoviesNetworkDataSource.kt
@@ -1,20 +1,23 @@
 package com.example.crearrepositorio.features.films.data
 
+import com.example.crearrepositorio.features.films.data.dto.MovieDTO
 import com.example.crearrepositorio.features.films.data.mapper.toMovieModel
 import com.example.crearrepositorio.features.films.data.network.MoviesService
 import com.example.crearrepositorio.features.films.domain.model.MovieModel
+import com.example.crearrepositorio.features.series.data.SeriesDTO
+import com.example.crearrepositorio.features.series.data.network.SeriesService
 import jakarta.inject.Inject
 
 class MoviesNetworkDataSource @Inject constructor(
-    private val moviesService: MoviesService
-) {
-    suspend fun fetchPopularMovies() : List<MovieModel> {
-        val response = moviesService.getPopularMovies()
+    private val moviesService: MoviesService){
 
-        if (!response.isSuccessful){
-            throw Exception("Error: ${response.code()} ${response.message()}")
+    suspend fun fetchPopularMovies(): List<MovieModel> {
+        val response = moviesService.getPopularMovies()
+        if (!response.isSuccessful) {
+            throw Exception("Error al obtener las peliculas")
         }else{
-            return response.body()?.results?.map { movie -> movie.toMovieModel()} ?: emptyList()
+            return response.body()?.results?.map { it.toMovieModel() } ?: emptyList()
+
         }
     }
 }
