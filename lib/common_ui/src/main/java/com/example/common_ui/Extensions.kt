81a293d754130c7632590e82a53652f13d0ea40e
mariosima1 <mariosima100@gmail.com>
2025-05-28 15:05:32 +0200

    main activity xml moved to common_ui

--- lib/common_ui/src/main/java/com/example/common_ui/Extensions.kt
+++ lib/common_ui/src/main/java/com/example/common_ui/Extensions.kt
@@ -3,10 +3,9 @@ package com.example.common_ui
 import androidx.fragment.app.Fragment
 
 fun Fragment.replaceFragment(fragment: Fragment) {
-    val containerId = (activity as FragmentContainerProvider).getFragmentContainerId()
 
     parentFragmentManager.beginTransaction()
-        .replace(containerId, fragment)
+        .replace(R.id.navHostFragment, fragment)
         .addToBackStack("CambioDeFragment")
         .commit()
 }
--- lib/common_ui/src/main/java/com/example/common_ui/FragmentContainerProvider.kt
+++
@@ -1,5 +0,0 @@
-package com.example.common_ui
-
-interface FragmentContainerProvider {
-    fun getFragmentContainerId(): Int
-}
